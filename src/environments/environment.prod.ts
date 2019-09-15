edae7b839d1498a199ad0b5b3f8e4b5760aa69cc
Antonio P Lacerda <antonioplacerda@gmail.com>
2019-09-15 17:51:47 +0100

    add firebase config

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: 'AIzaSyDKOR8V4mix0OeP-crlvb8mKTuo_Gmhtyw',
+    authDomain: 'quizzapp-fae38.firebaseapp.com',
+    databaseURL: 'https://quizzapp-fae38.firebaseio.com',
+    projectId: 'quizzapp-fae38',
+    storageBucket: '',
+    messagingSenderId: '914150753621',
+    appId: '1:914150753621:web:70b8391fd696ad38b2dbdc',
+  },
 };
