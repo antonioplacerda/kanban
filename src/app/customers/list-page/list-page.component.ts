3e972f520b21800b91297e7e1ce2b009ec54e86b
Antonio P Lacerda <antonioplacerda@gmail.com>
2019-09-26 21:47:39 +0100

    create list page

--- src/app/customers/list-page/list-page.component.ts
+++ src/app/customers/list-page/list-page.component.ts
@@ -1,15 +1,26 @@
 import { Component, OnInit } from '@angular/core';
+import { AngularFirestore } from '@angular/fire/firestore';
+import { SeoService } from 'src/app/services/seo.service';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-list-page',
   templateUrl: './list-page.component.html',
-  styleUrls: ['./list-page.component.scss']
+  styleUrls: ['./list-page.component.scss'],
 })
 export class ListPageComponent implements OnInit {
+  customers: Observable<any>;
 
-  constructor() { }
+  constructor(private seo: SeoService, private db: AngularFirestore) {}
 
   ngOnInit() {
-  }
+    this.seo.generateTags({
+      title: 'Customer List',
+      description: 'A list filled with customers',
+    });
 
+    this.customers = this.db
+      .collection('customers')
+      .valueChanges({ idField: 'id' });
+  }
 }
