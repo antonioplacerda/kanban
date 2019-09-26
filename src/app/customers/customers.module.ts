c70d65f1f2218a69c70eaf1b194970add4c30bcd
Antonio P Lacerda <antonioplacerda@gmail.com>
2019-09-26 21:47:10 +0100

    include shared module

--- src/app/customers/customers.module.ts
+++ src/app/customers/customers.module.ts
@@ -4,13 +4,10 @@ import { CommonModule } from '@angular/common';
 import { CustomersRoutingModule } from './customers-routing.module';
 import { ListPageComponent } from './list-page/list-page.component';
 import { DetailPageComponent } from './detail-page/detail-page.component';
-
+import { SharedModule } from '../shared/shared.module';
 
 @NgModule({
   declarations: [ListPageComponent, DetailPageComponent],
-  imports: [
-    CommonModule,
-    CustomersRoutingModule
-  ]
+  imports: [CommonModule, CustomersRoutingModule, SharedModule],
 })
-export class CustomersModule { }
+export class CustomersModule {}
